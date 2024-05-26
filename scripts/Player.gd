10b73155dfc6c49fe1b2d50cff8037b9a6da57dc
beatmil <anutorn@gmail.com>
2024-05-26 14:39:17 +0100

    Player can now receive fireball

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -58,3 +58,9 @@ func _on_block_button_pressed():
 func _on_charge_button_pressed():
 	chosen_action = ActionEnum.actions.CHARGE
 	$ActionLabel.text = ActionEnum.actions.keys()[chosen_action]
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("fireball"):
+		body.explode()
+		$"AnimationPlayer".play("hitted")
