f5a6a3f041cc4353f73cad7e40b5c438719bc2b6
beatmil <anutorn@gmail.com>
2024-05-26 13:43:35 +0100

    Make fireball able to going both side

--- scripts/fireball.gd
+++ scripts/fireball.gd
@@ -1,10 +1,14 @@
 extends RigidBody2D
 
+var is_going_right_side = true
+var speed = 20000
+
 
 func _ready():
-	apply_central_force(Vector2(20000, 0))
-
-
+	if is_going_right_side:
+		apply_central_force(Vector2(speed, 0))
+	else:
+		apply_central_force(Vector2(-speed, 0))
 
 
 func explode():
