9ee25d9b46505f69ef35d2d6f74a281be553b1c5
beatmil <anutorn@gmail.com>
2024-05-31 15:04:59 +0100

    Add restart_menu.tscn

---
+++ scripts/restart_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+@export var current_scene: String
+
+
+func _on_button_pressed():
+	SceneTransition.change_scene(current_scene)
+
+
+func open():
+	$"AnimationPlayer".play("fade_in")
