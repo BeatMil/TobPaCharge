d46d1393555cf25d8eb0ec0d46b4e88cf75d65b1
beatmil <anutorn@gmail.com>
2024-06-11 21:30:45 +0100

    Add chat system

--- scripts/steam_lobby.gd
+++ scripts/steam_lobby.gd
@@ -42,7 +42,8 @@ Nodes
 @onready var vbox_member: VBoxContainer = $"VBoxMember"
 @onready var create_lobby_button: Button = $"CreateLobbyButton"
 @onready var leave_lobby_button: Button = $"LeaveLobbyButton"
-@onready var lobby_text_edit: TextEdit = $"LobbyIdTextEdit"
+@onready var lobby_line_edit: LineEdit = $"LobbyIdLineEdit"
+@onready var chat_label: Label = $"ChatLabel"
 
 
 
@@ -50,7 +51,8 @@ func _ready() -> void:
 	# Steam.connect("avatar_loaded", _on_avatar_loaded)
 	Steam.connect("lobby_created", _on_lobby_created)
 	Steam.connect("lobby_joined", _on_lobby_joined)
-	Steam.connect("lobby_chat_update", _on_lobby_chat_update)
+	Steam.connect("lobby_chat_update", _on_lobby_chat_update) # when other player join
+	Steam.connect("lobby_message", _on_lobby_message)
 
 	_initialize_steam() # check if steam is running
 
@@ -98,9 +100,6 @@ func _initialize_steam() -> void:
 		compiled Godot version including GodotSteam / Steamworks.\n\n
 		For more information, visit https://godotsteam.com/")
 
-#################################################
-# CALLBACKS
-#################################################
 
 # # Avatar is ready for display
 # func _on_avatar_loaded(id: int, avatar_size: int, buffer: PackedByteArray) -> void:
@@ -150,34 +149,7 @@ func create_lobby() -> void:
 		# Set the lobby to public with ten members max
 		Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, lobby_max_members)
 	else:
-		printerr("Lobby already created??")
-
-
-# A lobby has been successfully created
-func _on_lobby_created(connect_result: int, _lobby_id: int) -> void:
-	if connect_result == 1:
-		print("[STEAM] Created a lobby: "+str(lobby_id)+"\n")
-
-		# Set lobby joinable as a test
-		var SET_JOINABLE: bool = Steam.setLobbyJoinable(lobby_id, true)
-		print("[STEAM] The lobby has been set joinable: "+str(SET_JOINABLE))
-
-
-		# Set some lobby data
-		var SET_LOBBY_DATA = Steam.setLobbyData(lobby_id, "name",
-			str(Steam.getPersonaName())+"'s Lobby")
-		print("[STEAM] Setting lobby name data successful: "+str(SET_LOBBY_DATA))
-
-		# Allow P2P connections to fallback to being relayed through Steam if needed
-		var IS_RELAY: bool = Steam.allowP2PPacketRelay(true)
-		print("[STEAM] Allowing Steam to be relay backup: "+str(IS_RELAY)+"\n")
-
-		# Disable create lobby button
-		create_lobby_button.disabled = true
-		leave_lobby_button.disabled = false
-	else:
-		print("[STEAM] Failed to create lobby\n")
-
+		printerr("Lobby create failed.")
 
 
 # Get the lobby members from Steam
@@ -236,6 +208,63 @@ func leave_lobby() -> void:
 func _on_leave_lobby_button_pressed():
 	leave_lobby()
 
+
+func _on_join_lobby_button_pressed():
+	Steam.joinLobby(int(lobby_line_edit.text))
+
+
+func _on_lobby_id_line_edit_text_submitted(new_text):
+	Steam.joinLobby(int(new_text))
+
+
+func _on_chat_line_edit_text_submitted(new_text):
+	# var MESSAGE: String = $Frame/Main/Messaging/Chat.get_text()
+	var IS_SENT: bool = Steam.sendLobbyChatMsg(lobby_id, new_text)
+	if not IS_SENT:
+		printerr("Sent message failed")
+	else:
+		print("message sent!: ", new_text)
+
+
+#################################################
+# CALLBACKS
+#################################################
+
+# When a lobby message is received
+func _on_lobby_message(_result: int, user: int, message: String, type: int) -> void:
+	print("▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦") 
+	print("%s: %s"%[Steam.getFriendPersonaName(user), message])
+	print("▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦") 
+	pass
+	# process_lobby_message(_result, user, message, type)
+
+
+# A lobby has been successfully created
+func _on_lobby_created(connect_result: int, _lobby_id: int) -> void:
+	if connect_result == 1:
+		print("[STEAM] Created a lobby: "+str(lobby_id)+"\n")
+
+		# Set lobby joinable as a test
+		var SET_JOINABLE: bool = Steam.setLobbyJoinable(lobby_id, true)
+		print("[STEAM] The lobby has been set joinable: "+str(SET_JOINABLE))
+
+
+		# Set some lobby data
+		var SET_LOBBY_DATA = Steam.setLobbyData(lobby_id, "name",
+			str(Steam.getPersonaName())+"'s Lobby")
+		print("[STEAM] Setting lobby name data successful: "+str(SET_LOBBY_DATA))
+
+		# Allow P2P connections to fallback to being relayed through Steam if needed
+		var IS_RELAY: bool = Steam.allowP2PPacketRelay(true)
+		print("[STEAM] Allowing Steam to be relay backup: "+str(IS_RELAY)+"\n")
+
+		# Disable create lobby button
+		create_lobby_button.disabled = true
+		leave_lobby_button.disabled = false
+	else:
+		print("[STEAM] Failed to create lobby\n")
+
+
 """
 This trigger both when create or join lobby.
 """
@@ -259,10 +288,13 @@ func _on_lobby_joined(_lobby_id: int, _permissions: int, _locked: bool, _respons
 func _on_lobby_chat_update(lobby_id: int, changed_id: int, making_change_id: int, chat_state: int) -> void:
 	# Note that chat state changes is: 1 - entered, 2 - left, 4 - user disconnected before leaving, 8 - user was kicked, 16 - user was banned
 	print("[STEAM] Lobby ID: "+str(lobby_id)+", Changed ID: "+str(changed_id)+", Making Change: "+str(making_change_id)+", Chat State: "+str(chat_state))
+
+	if chat_state == 1:
+		chat_label.text += ("[STEAM]  has joined the lobby.\n")
+		# Else if a player has left the lobby
+	elif chat_state == 2:
+		chat_label.text += ("[STEAM]  has left the lobby.\n")
 	# Update the lobby now that a change has occurred
 	get_lobby_members()
 
 
-func _on_join_lobby_button_pressed():
-	var lobby_id = int(lobby_text_edit.text)
-	Steam.joinLobby(lobby_id)
