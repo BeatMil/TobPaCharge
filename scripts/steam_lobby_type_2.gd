e2b31398c1f988e38882906a601da0d50d52bcfe
beatmil <anutorn@gmail.com>
2024-06-14 16:17:34 +0100

    Test mul4

--- scripts/steam_lobby_type_2.gd
+++ scripts/steam_lobby_type_2.gd
@@ -30,10 +30,13 @@ var lobby_id: int = 0
 var lobby_members: Array = []
 var lobby_max_members: int = 4
 
+@onready var create_lobby_button: Button = $"CreateLobbyButton"
+@onready var lobby_id_label: RichTextLabel = $"LobbyIdLabel"
+@onready var vbox_member: VBoxContainer = $"VBoxMember"
+
 """
 Steam Multiplayer thingy
 """
-
 var steam_multiplayer: SteamMultiplayerPeer = SteamMultiplayerPeer.new()
 
 """
@@ -45,26 +48,17 @@ Preloads
 """
 Nodes
 """
-@onready var lobby_id_label: RichTextLabel = $"LobbyIdLabel"
-@onready var vbox_member: VBoxContainer = $"VBoxMember"
-@onready var create_lobby_button: Button = $"CreateLobbyButton"
-@onready var leave_lobby_button: Button = $"LeaveLobbyButton"
-@onready var lobby_line_edit: LineEdit = $"LobbyIdLineEdit"
-@onready var chat_label: RichTextLabel = $"AvatarPanel/ChatLabel"
-@onready var chat_line_edit: LineEdit = $"ChatLineEdit"
-
 
 func _ready() -> void:
 	# Steam.connect("avatar_loaded", _on_avatar_loaded)
-	# Steam.connect("lobby_created", _on_lobby_created)
+	#Steam.connect("lobby_created", _on_lobby_created)
 	# Steam.connect("lobby_joined", _on_lobby_joined)
-	# Steam.connect("join_requested", _on_join_requested)
+	Steam.connect("join_requested", _on_join_requested)
 	# Steam.connect("lobby_chat_update", _on_lobby_chat_update) # when other player join
 	# Steam.connect("lobby_message", _on_lobby_message)
 	# Steam.connect("lobby_invite", _on_lobby_invite)
 
-	multiplayer.connect("peer_connected", _peer_connected)
-	multiplayer.connect("peer_disconnected", _peer_disconnected)
+
 	steam_multiplayer.connect("lobby_created", _on_lobby_created)
 	steam_multiplayer.connect("lobby_joined", _on_lobby_joined)
 
@@ -115,45 +109,6 @@ func _initialize_steam() -> void:
 		For more information, visit https://godotsteam.com/")
 
 
-func _on_button_pressed():
-	print("bob")
-	if is_online:
-		print("bob2")
-		$"UsernameLabel".text = Steam.getPersonaName()
-		$"UserLevelLabel".text = str(Steam.getPlayerSteamLevel())
-
-
-func _on_button_2_pressed():
-	Steam.getPlayerAvatar(Steam.AVATAR_LARGE, Steam.getSteamID())
-
-
-func _on_button_3_pressed():
-	Steam.getPlayerAvatar(Steam.AVATAR_MEDIUM, Steam.getSteamID())
-
-
-func _on_button_4_pressed():
-	Steam.getPlayerAvatar(Steam.AVATAR_SMALL, Steam.getSteamID())
-
-
-#################################################
-# LOBBY
-#################################################
-
-func _on_create_lobby_button_pressed():
-	create_lobby()
-
-
-# When the user starts a game with multiplayer enabled
-func create_lobby() -> void:
-	# Make sure a lobby is not already set
-	if lobby_id == 0:
-		# Set the lobby to public with ten members max
-		#Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, lobby_max_members)
-		steam_multiplayer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_FRIENDS_ONLY, 4)
-	else:
-		printerr("Lobby create failed.")
-
-
 # Get the lobby members from Steam
 func get_lobby_members() -> void:
 	# Clear your previous lobby list
@@ -195,74 +150,30 @@ func get_lobby_members() -> void:
 		mul_test.position = $"SpawnPoints".get_children()[MEMBER].position
 		$"ColorPanels".add_child(mul_test)
 
-		# Steam.getPlayerAvatar(Steam.AVATAR_MEDIUM, MEMBER_STEAM_ID) nice?
-		# # Add them to the player list
-		# add_player_to_connect_list(MEMBER_STEAM_ID, MEMBER_STEAM_NAME)
+
+func get_lobby_members_at_home() -> void:
+	# Get the number of members from this lobby from Steam
+	var MEMBERS: int = Steam.getNumLobbyMembers(lobby_id)
+	# Update the player list title
+	print("Player List ("+str(MEMBERS)+")")
 
 
-func leave_lobby() -> void:
-	if lobby_id != 0:
-		Steam.leaveLobby(lobby_id)
-		print("Left the lobby")
-		
-		# Reset lobby_id
-		lobby_id = 0
-		# clear Vboxmember
-		for node in vbox_member.get_children():
-			node.queue_free()
-
-		# Set label
-		lobby_id_label.text = ""
-
-		# Set buttons
-		create_lobby_button.disabled = false
-		leave_lobby_button.disabled = true
-
-
-func _on_leave_lobby_button_pressed():
-	leave_lobby()
-
-
-func _on_join_lobby_button_pressed():
-	Steam.joinLobby(int(lobby_line_edit.text))
-
-
-func _on_lobby_id_line_edit_text_submitted(new_text):
-	Steam.joinLobby(int(new_text))
-
-
-func _on_chat_line_edit_text_submitted(new_text):
-	var IS_SENT: bool = Steam.sendLobbyChatMsg(lobby_id, new_text)
-	if not IS_SENT:
-		printerr("Sent message failed")
-	else:
-		print("message sent!: ", new_text)
-		chat_line_edit.text = ""
-
-
-#################################################
-# CALLBACKS
-#################################################
-
-# When a lobby message is received
-func _on_lobby_message(_lobby_id: int, user: int, message: String, type: int) -> void:
-	chat_label.text += "%s: %s\n"%[Steam.getFriendPersonaName(user), message]
-	print("▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦") 
-	print("%s %s: %s, %s"%[_lobby_id, Steam.getFriendPersonaName(user), message, type])
-	print("▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦") 
-	pass
-	# process_lobby_message(_result, user, message, type)
+func create_lobby() -> void:
+	var error = steam_multiplayer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_FRIENDS_ONLY, 4)
+	if error:
+		printerr("Create lobby failed")
+	#Steam.createLobby(Steam.LOBBY_TYPE_FRIENDS_ONLY, lobby_max_members)
 
 
 # A lobby has been successfully created
 func _on_lobby_created(connect_result: int, _lobby_id: int) -> void:
 	if connect_result == 1:
+		lobby_id = _lobby_id
 		print("[STEAM] Created a lobby: "+str(lobby_id)+"\n")
 
 		# Set lobby joinable as a test
 		var SET_JOINABLE: bool = Steam.setLobbyJoinable(lobby_id, true)
 		print("[STEAM] The lobby has been set joinable: "+str(SET_JOINABLE))
-
 
 		# Set some lobby data
 		var SET_LOBBY_DATA = Steam.setLobbyData(lobby_id, "name",
@@ -274,8 +185,7 @@ func _on_lobby_created(connect_result: int, _lobby_id: int) -> void:
 		print("[STEAM] Allowing Steam to be relay backup: "+str(IS_RELAY)+"\n")
 
 		# Disable create lobby button
-		create_lobby_button.disabled = true
-		leave_lobby_button.disabled = false
+		# create_lobby_button.disabled = true
 	else:
 		print("[STEAM] Failed to create lobby\n")
 
@@ -294,40 +204,12 @@ func _on_lobby_joined(_lobby_id: int, _permissions: int, _locked: bool, _respons
 		get_lobby_members()
 		# Disable create lobby button
 		create_lobby_button.disabled = true
-		leave_lobby_button.disabled = false
-
+		# leave_lobby_button.disabled = false
 	else:
 		printerr("Create or Join lobby failed")
-
-
-# When a lobby chat is updated
-func _on_lobby_chat_update(lobby_id: int, changed_id: int, making_change_id: int, chat_state: int) -> void:
-	# Note that chat state changes is: 1 - entered, 2 - left, 4 - user disconnected before leaving, 8 - user was kicked, 16 - user was banned
-	print("[STEAM] Lobby ID: "+str(lobby_id)+", Changed ID: "+str(changed_id)+", Making Change: "+str(making_change_id)+", Chat State: "+str(chat_state))
-
-	if chat_state == 1:
-		chat_label.text += ("[STEAM]  has joined the lobby.\n")
-		# Else if a player has left the lobby
-	elif chat_state == 2:
-		chat_label.text += ("[STEAM]  has left the lobby.\n")
-	# Update the lobby now that a change has occurred
-	get_lobby_members()
-
-
-# Trigger when got a friend invite from steam
-func _on_lobby_invite(inviter: int, lobby: int, game: int):
-	print("%s, %s, %s"%[inviter, lobby, game])
 
 
 # Trigger when accepct friend invite from steam
 func _on_join_requested(_lobby_id: int, _steam_id: int):
 	steam_multiplayer.connect_lobby(_lobby_id)
 	#Steam.joinLobby(_lobby_id)
-
-
-func _peer_connected(_id :int):
-	print("=============connected peer id: %s"%_id)
-
-
-func _peer_disconnected(_id :int):
-	print("=============disconnected peer id: %s"%_id)
