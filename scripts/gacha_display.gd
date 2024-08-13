5ac52383485cc1fa9a4f47ea5a9126a0e1e012bc
beatmil <anutorn@gmail.com>
2024-08-13 20:43:39 +0100

    Fix gacha doesn't display correct cosmetics

--- scripts/gacha_display.gd
+++ scripts/gacha_display.gd
@@ -5,6 +5,7 @@ extends Node2D
 ## Preloads
 #############################################################
 const BOWTIE_TYPE = preload("res://nodes/cosmetics/bowtie_type.tscn")
+const HIPS_TYPE = preload("res://nodes/cosmetics/hips_type.tscn")
 const HIT_SPARK_FIREBALL = preload("res://nodes/particles_effects/hit_spark_fireball.tscn")
 
 
@@ -20,7 +21,6 @@ const HIT_SPARK_FIREBALL = preload("res://nodes/particles_effects/hit_spark_fire
 #############################################################
 func _ready() -> void:
 	run_gacha()
-	print(Data.inventory["gura_blue"])
 
 
 #############################################################
@@ -29,7 +29,11 @@ func _ready() -> void:
 func run_gacha() -> void:
 	var the_thing = _random_gacha()
 	var parts = the_thing.split("_")
-	_spawn_bowtie_gacha(parts[0], parts[1])
+	var real_type = _determine_cosmetic_type(parts[0])
+	if real_type == "bowtie":
+		_spawn_bowtie_gacha(parts[0], parts[1])
+	elif real_type == "hips":
+		_spawn_hips_gacha(parts[0], parts[1])
 	animation_player.play("run_gacha")
 	label_player.play("pop_in")
 
@@ -61,6 +65,27 @@ func _spawn_bowtie_gacha(_type, _color) -> void:
 	bowtie.color = _color
 	add_child(bowtie)
 	bowtie.play_dance()
+
+
+func _spawn_hips_gacha(_type, _color) -> void:
+	var bowtie = HIPS_TYPE.instantiate()
+	bowtie.bowtie_type = _type
+	bowtie.color = _color
+	add_child(bowtie)
+	bowtie.play_dance()
+
+
+func _determine_cosmetic_type(_cosmetic: String) -> String:
+	var bowtie_type = ["bowtie", "gura", "skull"]
+	var hips_type = ["gun", "kunia", "pouch"]
+
+	if _cosmetic in bowtie_type:
+		return "bowtie"
+	
+	if _cosmetic in hips_type:
+		return "hips"
+	
+	return "Error"
 
 
 ## Used by AnimationPlayer
